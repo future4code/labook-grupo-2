d146f984b3051e69d7c8a3f202d356e6021c341d
Rosana Rezende <rezende_rosana@hotmail.com>
2020-05-19 14:01:01 -0300

    adaptando projeto ao MVC

---
+++ src/business/UserBusiness.ts
@@ -0,0 +1,36 @@
+import { IdGenerator } from '../services/IdGenerator'
+import { HashManager } from '../services/HashManager'
+import { UserDatabase } from '../data/UserDatabase'
+
+export class UserBusiness {
+
+    public async signup(email: string, name: string, password: string, role: string){
+        const idGenerator = new IdGenerator()
+        const id = idGenerator.generatorId()
+
+        const hashManager = new HashManager()
+        const hashPassword = await hashManager.hash(password)
+
+        const userDatabase = new UserDatabase()
+        await userDatabase.createUser(id, email, name, hashPassword, role)
+
+        return { id: id, role: role }
+    }
+
+    public async login(email: string, password: string){
+        const userDataBase = new UserDatabase()
+        const user = await userDataBase.getUserByEmail(email)
+
+        const hashManager = new HashManager()
+        const comparePasswords = await hashManager.compare(password, user.password)
+
+        if (!comparePasswords) {
+            throw new Error("Invalid Params")
+        }
+        return { id: user.id, role: user.role}
+
+    }
+    
+
+
+}
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,14 @@
 import express from "express";
 import { AddressInfo } from "net";
 import dotenv from "dotenv";
-import { signupEndpoint } from "./endpoint/signupEndpoint";
-import { loginEndPoint } from "./endpoint/loginEndPoint";
+import { userRouter } from "./router/UserRouter"
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 
-app.post("/signup", signupEndpoint)
-app.post("/login", loginEndPoint)
+app.use("/user/", userRouter)
 
-// =======================================================
 
 
 const server = app.listen(process.env.PORT || 3000, () => {
