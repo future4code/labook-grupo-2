8643624be1d3e6e77856dd1a1442d2822f793b14
ThalesMilanezi <milanezitf@gmail.com>
2020-05-27 14:30:52 -0300

    refactor de createuser completo

--- src/data/UserDatabase.ts
+++ src/data/UserDatabase.ts
@@ -4,21 +4,26 @@ import { User } from '../model/User'
 export class UserDatabase extends BaseDatabase {
     public static TABLE_NAME = 'LaBookUser'
 
-    private toModel(dbResult?:any):User | undefined{
+    private toModel(dbResult?: any): User | undefined {
         return (
             dbResult && new User(dbResult.id, dbResult.email, dbResult.name, dbResult.password, dbResult.role)
         )
     }
 
-    public async createUser(id: string, email: string, name: string, password: string, role: string): Promise<void> {
+    public async createUser(user: User): Promise<void> {
+        const userData = this.toModel(user)
         await this.connection()
             .insert({
-                id, email, name, password, role
+                id: userData?.getId(),
+                email: userData?.getEmail(),
+                name: userData?.getName(),
+                password: userData?.getPassword(),
+                role: userData?.getRole()
             })
             .into(UserDatabase.TABLE_NAME)
     }
 
-    public async getUserByEmail(email: string): Promise<User | undefined>  {
+    public async getUserByEmail(email: string): Promise<User | undefined> {
         const result = await this.connection()
             .select("*")
             .from(UserDatabase.TABLE_NAME)
